3fcbdd0a397aa6b89a719862472633ceea45f88c
zk <zk@hahaha.com>
2016-09-13 16:11:00 +0800

    delete the bloody thing

--- src/com/google/gwt/sample/stockwatcher/server/GreetingServiceImpl.java
+++ src/com/google/gwt/sample/stockwatcher/server/GreetingServiceImpl.java
@@ -1,26 +1,14 @@
 package com.google.gwt.sample.stockwatcher.server;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.OutputStream;
 import java.net.Socket;
-import java.security.MessageDigest;
-import java.security.spec.KeySpec;
 import java.sql.Date;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-
-import javax.crypto.Cipher;
-import javax.crypto.SealedObject;
-import javax.crypto.SecretKey;
-import javax.crypto.SecretKeyFactory;
-import javax.crypto.spec.PBEKeySpec;
-import javax.crypto.spec.SecretKeySpec;
 
 import com.google.gwt.sample.stockwatcher.client.GreetingService;
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
@@ -30,16 +18,6 @@ import com.google.gwt.user.server.rpc.RemoteServiceServlet;
  */
 @SuppressWarnings("serial")
 public class GreetingServiceImpl extends RemoteServiceServlet implements GreetingService {
-	
-	public static String hashSHA1CharAry (String c) {
-    	try {
-    		MessageDigest md=MessageDigest.getInstance("SHA-1");
-    		md.reset();
-    		md.update(c.getBytes());
-    		return new String(md.digest());
-    	} catch (Exception e) {}
-    	return "";
-    }
 	
 	public String[][] greetServer(String sn, Date sd, Date ed) throws IllegalArgumentException {
 		try {
@@ -52,12 +30,12 @@ public class GreetingServiceImpl extends RemoteServiceServlet implements Greetin
 			toSend.add(LocalDateTime.now());
 			OutputStream os = sc.getOutputStream();
 			ObjectOutputStream oos=new ObjectOutputStream(os);
-			oos.writeObject(toSend);
+			oos.writeObject(Utilities.encryptMsg(toSend));
 			
 			InputStream is=sc.getInputStream();
 			ObjectInputStream ois=new ObjectInputStream(is);
 			@SuppressWarnings("unchecked")
-			ArrayList<Object []> data=(ArrayList<Object []>)ois.readObject();
+			ArrayList<Object []> data=Utilities.decryptData(ois);
 			
 			oos.close();
 			os.close();
@@ -66,32 +44,14 @@ public class GreetingServiceImpl extends RemoteServiceServlet implements Greetin
 			
 			sc.close();
 			
-			DateTimeFormatter df = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-			String [][] newData = new String[data.size()][2];
-			
-			for(int i=0;i<data.size();i++)
-			{
-		    	newData[i][0]=df.format((LocalDateTime)data.get(i)[0]);
-		    	newData[i][1]=data.get(i)[1].toString();
-			}
-			
-			return newData;
+			return Utilities.DataToString(data);
 		} catch (Exception e) {return null;}
 	}
 	
 	public boolean userLogin(String username, String password) throws IllegalArgumentException {
 		try {
-
-	        final byte[] salt = "chichilol".getBytes();
-	        SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
-	        KeySpec spec = new PBEKeySpec("chi-admin".toCharArray(), salt, 1024, 128);
-	        SecretKey secret=new SecretKeySpec(factory.generateSecret(spec).getEncoded(),"AES");
-	        Cipher encrypter=Cipher.getInstance("AES");
-	        encrypter.init(Cipher.ENCRYPT_MODE,secret);
-	        Cipher decrypter=Cipher.getInstance("AES");
-	        decrypter.init(Cipher.DECRYPT_MODE,secret);
 			
-			String hashedPassword = hashSHA1CharAry(password);
+			String hashedPassword = Utilities.hashSHA1CharAry(password);
 			Socket sc=new Socket("10.100.2.56",40001);
 			ArrayList<Object> toSend=new ArrayList<>();
 			toSend.add("2");
@@ -99,12 +59,12 @@ public class GreetingServiceImpl extends RemoteServiceServlet implements Greetin
 			toSend.add(hashedPassword);
 			OutputStream os = sc.getOutputStream();
 			ObjectOutputStream oos=new ObjectOutputStream(os);
-			oos.writeObject(new SealedObject(toSend,encrypter));
+			oos.writeObject(Utilities.encryptMsg(toSend));
 			
 			InputStream is=sc.getInputStream();
 			ObjectInputStream ois=new ObjectInputStream(is);
 			@SuppressWarnings("unchecked")
-			String data=(String)(((SealedObject) ois.readObject()).getObject(decrypter));
+			String data=Utilities.decryptMsg(ois);
 			
 			oos.close();
 			os.close();
@@ -117,4 +77,5 @@ public class GreetingServiceImpl extends RemoteServiceServlet implements Greetin
 		} catch (Exception e) {return false;}
 	}
 
+	
 }
---
+++ src/com/google/gwt/sample/stockwatcher/server/Utilities.java
@@ -0,0 +1,95 @@
+package com.google.gwt.sample.stockwatcher.server;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.security.MessageDigest;
+import java.security.spec.KeySpec;
+import java.sql.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+
+import javax.crypto.Cipher;
+import javax.crypto.SealedObject;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
+
+import com.google.gwt.sample.stockwatcher.client.GreetingService;
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+
+/**
+ * The server-side implementation of the RPC service.
+ */
+@SuppressWarnings("serial")
+public class Utilities{
+	
+	public static String hashSHA1CharAry (String c) {
+    	try {
+    		MessageDigest md=MessageDigest.getInstance("SHA-1");
+    		md.reset();
+    		md.update(c.getBytes());
+    		return new String(md.digest());
+    	} catch (Exception e) {}
+    	return "";
+    }
+	
+	public static SecretKey getKey(){
+		try {
+			final byte[] salt = "chichilol".getBytes();
+			SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+			KeySpec spec = new PBEKeySpec("chi-admin".toCharArray(), salt, 1024, 128);
+			SecretKey secret=new SecretKeySpec(factory.generateSecret(spec).getEncoded(),"AES");
+		
+			return secret;
+		} catch (Exception e) {return null;}
+	}
+
+	public static SealedObject encryptMsg(ArrayList<Object> msg){
+		try {
+			Cipher encrypter=Cipher.getInstance("AES");
+	        encrypter.init(Cipher.ENCRYPT_MODE,getKey());
+	        
+			return new SealedObject(msg,encrypter);
+		} catch (Exception e) {return null;}
+	}
+	
+	public static String decryptMsg(ObjectInputStream ois){
+		try {
+	        Cipher decrypter=Cipher.getInstance("AES");
+	        decrypter.init(Cipher.DECRYPT_MODE,getKey());
+	        
+			return (String)(((SealedObject) ois.readObject()).getObject(decrypter));
+		} catch (Exception e) {return null;}
+	}
+	
+	@SuppressWarnings("unchecked")
+	public static ArrayList<Object []> decryptData(ObjectInputStream ois){
+		try {
+			return (ArrayList<Object []>)ois.readObject();
+		} catch (Exception e) {return null;}
+	}
+	
+	public static String[][] DataToString(ArrayList<Object []> input){
+		try {
+			DateTimeFormatter df = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+			String [][] data = new String[input.size()][2];
+			
+			for(int i=0;i<input.size();i++)
+			{
+		    	data[i][0]=df.format((LocalDateTime)input.get(i)[0]);
+		    	data[i][1]=input.get(i)[1].toString();
+			}
+			
+			return data;
+		} catch (Exception e) {return null;}
+	}
+	
+}
